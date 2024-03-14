e1e60db63c4204242644466278b2049e981880dc
axe_mello <akselcruses@gmail.com>
2024-03-14 20:34:22 +0530

    js

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ function global () {
     local();
 }
 global();
+
+var string = "Hello";
+string += " World";
+console.log(string + "!");
+
+var x = 5, y = 2;
+var z = x + y /2;
+console.log(z);
+
+x = 200;
+y = "100";
+
+if ( x >= y || x <= y) {
+    console.log("hot or cold?");
+}
+else {
+    console.log("bingo!");
+}
+
+function name () {
+    return {
+        name : "Aksel"
+    };
+}
+console.log(name());    
+
+//for loop
+sum = 0;
+for (var i = 0; i <10; i++) {
+    sum += i;
+}
+console.log(sum);
