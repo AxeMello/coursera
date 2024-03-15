08f2a12b79083535fcf1b0abb99b5d28ac99c134
axe_mello <akselcruses@gmail.com>
2024-03-15 20:34:23 +0530

    js js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function compare (x, y) {
     return x > y;
 }
-var x = compare (5,3);
+// var x = compare (5,3);
+console.log(compare(6,3));
 
 function global () {
     var message = "global";
@@ -45,3 +46,60 @@ for (var i = 0; i <10; i++) {
     sum += i;
 }
 console.log(sum);
+
+
+var a = new Object();
+a.name = new Object;
+a.name.fullname = "Aksel Cruses";
+a.name["weight count"] = 64;
+
+console.log(a);
+console.log(a.name.fullname);
+console.log(a.name["weight count"]);
+
+var better = {
+    name: "Aksel",
+    birthday: "9th October",
+    status: "single",
+    color: {
+        first: "black",
+        second: "gray",
+    },
+    number: 9 
+};
+
+console.log(window.better);
+
+
+function makemul(smthn) {
+    var func = function(x) {
+        return smthn * x;
+    };
+    return func
+};
+
+var make5 = makemul(5);
+console.log(make5(3));
+
+var a = 5;
+var b = 20;
+var c = {x: 5};
+
+a = b;
+console.log(a);
+b.x = 7;
+console.log(b);
+console.log(a);
+
+// Function constructors
+function Circle (radius) { 
+    this.radius = radius;
+}
+Circle.prototype.getArea =
+    function () {
+        return Math.PI * Math.pow(this.radius, 2);
+    };
+    
+var myCircle = new Circle(10);
+console.log(myCircle.getArea());
+var myOtherCircle = new Circle(20); console.log(myOtherCircle);
